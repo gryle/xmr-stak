99258bfd8ec8b1d8981cc63525458d412f8465a4
Nuno Grilo <nuno.grilo@gmail.com>
2018-04-08 23:30:48 +0100

    Fix for cuda compile

--- CompileIt.cmd
+++ CompileIt.cmd
@@ -1,5 +1,7 @@
 @echo off
 PUSHD %~DP0
+rem Execute next line only if compiling for Cuda 9.1 and using Visual Studio 2017 >= 15.5 (released 12/04/17)
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 -vcvars_ver=14.11
 echo ########## Running VsMSBuildCmd.bat
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
 echo ########## Setting CMAKE_PREFIX_PATH
@@ -10,7 +12,8 @@ mkdir build
 echo ########## Changing to build directory
 cd build
 echo ########## Running cmake -G "Visual Studio 15 2017 Win64" -T v141,host=x64 ..
-cmake -G "Visual Studio 15 2017 Win64" -T v141,host=x64 -DCUDA_ENABLE=OFF ..
+rem cmake -G "Visual Studio 15 2017 Win64" -T v141,host=x64 -DCUDA_ENABLE=OFF ..
+cmake -G "Visual Studio 15 2017 Win64" -T v141,host=x64 ..
 echo ########## Running cmake --build . --config Release --target install
 cmake --build . --config Release --target install
 echo ########## Changing to bin\Release directory
